db05ba26acb246c02c0e70050bfc5392ccd408d2
storyinvisible <storyinvisible@gmail.com>
2019-12-01 11:01:14 +0800

    Create_packages functions

---
+++ Trying_calendar_with events/app/src/main/java/com/example/trying_calendar/Create_packages.java
@@ -0,0 +1,14 @@
+package com.example.trying_calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_packages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_packages);
+    }
+}
